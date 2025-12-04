bd36ece640c516d3bb8acef0d4f5a8569ebdfb79
John Carlo M. Borlagdan <jcmborlagdan@headoffice.corp.bpi.com.ph>
2025-12-04 11:26:45 +0800

    Java Training - Module 1 Activity 5

---
+++ M1Activity5.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ph.com.bpi.module1;
+
+/**
+ * 
+ */
+public class M1Activity5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int sum = 0;
+		for (int i = 1; i <= 50; i++) {
+			sum += i;
+		}
+		System.out.println("Result: " + sum);
+	}
+
+}
