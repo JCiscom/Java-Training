bf3542eae20c7a686643b2760ab1d7024c8d2ce5
John Carlo M. Borlagdan <jcmborlagdan@headoffice.corp.bpi.com.ph>
2025-12-04 10:49:26 +0800

    Java Training - Module 1 Activity 1

---
+++ M1Activity1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ph.com.bpi.module1;
+import java.util.Scanner;
+/**
+ * 
+ */
+public class M1Activity1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("What is your name? ");
+		String name = input.nextLine();
+		System.out.println("Hello, " + name + "!");
+	}
+
+}
