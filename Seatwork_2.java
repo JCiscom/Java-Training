372426ccb5cab938e891ef0b1a9ef7f015e182d1
John Carlo M. Borlagdan <jcmborlagdan@headoffice.corp.bpi.com.ph>
2025-12-04 19:21:33 +0800

    Java Training - Module 2 Act 2

---
+++ Seatwork_2.java
@@ -0,0 +1,12 @@
+package Seatwork2;
+
+public class Seatwork_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhiteBoard wb = new WhiteBoard();
+		int result = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(result));
+	}
+
+}
