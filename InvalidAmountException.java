3f8440fe9eb77a1c28dcd4ca7d9b5b328c3dcb0d
John Carlo M. Borlagdan <jcmborlagdan@headoffice.corp.bpi.com.ph>
2026-01-15 17:19:19 +0800

    Java Training - Module 4 Activity 8

---
+++ InvalidAmountException.java
@@ -0,0 +1,13 @@
+package activity6;
+
+
+
+public class InvalidAmountException extends Exception {
+
+	public InvalidAmountException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+		//System.err.println(message);
+	}
+
+}
