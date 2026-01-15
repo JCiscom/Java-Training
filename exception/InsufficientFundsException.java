3e8ace4e5502c70e94531d3ea4693c6e84d498f4
John Carlo M. Borlagdan <jcmborlagdan@headoffice.corp.bpi.com.ph>
2026-01-15 17:22:43 +0800

    Java Training - Module 4 Activity 9

---
+++ exception/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package activity9.exception;
+
+public class InsufficientFundsException extends Exception {
+	
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+    
+}
---
+++ util/TestOperation.java
@@ -0,0 +1,8 @@
+package activity9.util;
+
+@FunctionalInterface
+public interface TestOperation {
+	
+	void execute() throws Exception;
+	
+}
